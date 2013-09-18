a7f9b478bedf908b03f0b8f6b101c969017670bc
Hyo Seong Choi <rorlab@gmail.com>
2013-09-18 10:19:35 +0900

    initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+angular.module('ngApp', [])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/items', {
+        templateUrl: 'views/items.html',
+        controller: 'ItemsCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,33 @@
+'use strict';
+
+angular.module('ngApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
+
+angular.module('ngApp')
+  .factory('itemFactory', function($http) {
+    // var items = [
+    //   { "description": "1"},
+    //   { "description": "1"},
+    //   { "description": "1"},
+    //   { "description": "1"},
+    //   { "description": "1"},
+    // ];
+    var items = $http.get('http://localhost:3000/items.json').success(function(data) {
+      debugger;
+      return data;
+    });
+    var factory = {}; 
+    factory.getItems = function() { 
+      return items; 
+    };
+    return factory; 
+  })
+  .controller('ItemsCtrl', function($scope, itemFactory) {
+    $scope.items = itemFactory.getItems();
+  });
