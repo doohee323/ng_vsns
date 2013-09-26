23f5b91e48abecc3a8ac91de7ff114f20dd4b14a
doohee <doohee323@gmail.com>
2013-09-26 22:12:54 +0900

    refactoring service

--- app/scripts/controllers/ngController.js
+++ app/scripts/controllers/ngController.js
@@ -1,77 +1,36 @@
 'use strict';
 
+/**
+ * MainCtrl 처리
+ * @param $scope
+ * @desc main.html 를 위한 controller
+ */
 app.controller('MainCtrl', function ($scope) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
-  })
-  .controller('LoginCtrl', function($scope){
+});
+
+/**
+ * LoginCtrl 처리
+ * @param $scope
+ * @desc login.html 를 위한 controller
+ */
+app.controller('LoginCtrl', function($scope){
     $scope.login = function() {
       if (this.email) {
         alert(this.email);
       }
     };
-  })
-  .controller('PostsCtrl', function($scope, $http) {
-    $http.get(config.url + 'posts.json')
-      .success(function(data, status, headers, config) {
-        // this callback will be called asynchronously
-        // when the response is available
-        $scope.posts = data;
-        $scope.formState = "Create";
-      })
-      .error(function(data, status, headers, config) {
-        // called asynchronously if an error occurs
-        // or server returns response with status
-        // code outside of the <200, 400) range
-      });
-    $scope.submit = function() {
-      if ($scope.formState == "Create"){
-        $http.post(config.url + 'posts', { post: $scope.post })
-          .success(function (data, status, headers, config) {
-            // TODO
-            $scope.posts.unshift( data );
-          }).error(function (data, status, headers, config) {
-            // TODO
-          });
+});
 
-      } else if ($scope.formState == "Update") {
-        $http.put(config.url + 'posts/' + $scope.post.id + '.json', { post: $scope.post })
-          .success(function (data, status, headers, config) {
-            for (var i in $scope.posts) {
-              if ($scope.posts[i].id == data.id) {
-                $scope.posts[i] = data;
-              }
-            }
-          }).error(function (data, status, headers, config) {
-            alert("failure");
-            // TODO
-          });
-      };
-      $scope.post = {};
-      $scope.formState = "Create";
-    };
-    $scope.deletePost = function(post){
-      if (confirm('Are you sure?') == true) {
-        $http.delete(config.url + 'posts/'+post.id  );
-        var index = $scope.posts.indexOf(post);
-        $scope.posts.splice(index,1);
-      }
-    };
-    $scope.editPost = function(post, index){
-      $http.get(config.url + 'posts/' + post.id + '.json')
-        .success(function(data, status, headers, config) {
-          // this callback will be called asynchronously
-          // when the response is available
-          $scope.post = data;
-          $scope.formState = "Update";
-        })
-        .error(function(data, status, headers, config) {
-          // called asynchronously if an error occurs
-          // or server returns response with status
-          // code outside of the <200, 400) range
-        });
-    };
-  });
+/**
+ * PostsCtrl 처리
+ * @param $scope, $http
+ * @desc posts.html 를 위한 controller
+ */
+app.controller('PostsCtrl', function($scope, $http, $location, postsService) {
+	postsService.main($scope, $http, $location);
+});
---
+++ app/scripts/services/postsService.js
@@ -0,0 +1,69 @@
+'use strict';
+
+app.service('postsService', function() {
+
+	this.main = function($scope, $http, $location) {
+		$http.get(config.url + 'posts.json')
+	    .success(function(data, status, headers, config) {
+	      // this callback will be called asynchronously
+	      // when the response is available
+	      $scope.posts = data;
+	      $scope.formState = "Create";
+	    })
+	    .error(function(data, status, headers, config) {
+	      // called asynchronously if an error occurs
+	      // or server returns response with status
+	      // code outside of the <200, 400) range
+	    });
+	  
+	  $scope.submit = function() {
+	    if ($scope.formState == "Create"){
+	      $http.post(config.url + 'posts', { post: $scope.post })
+	        .success(function (data, status, headers, config) {
+	          // TODO
+	          $scope.posts.unshift( data );
+	        }).error(function (data, status, headers, config) {
+	          // TODO
+	        });
+
+	    } else if ($scope.formState == "Update") {
+	      $http.put(config.url + 'posts/' + $scope.post.id + '.json', { post: $scope.post })
+	        .success(function (data, status, headers, config) {
+	          for (var i in $scope.posts) {
+	            if ($scope.posts[i].id == data.id) {
+	              $scope.posts[i] = data;
+	            }
+	          }
+	        }).error(function (data, status, headers, config) {
+	          alert("failure");
+	          // TODO
+	        });
+	    };
+	    $scope.post = {};
+	    $scope.formState = "Create";
+	  };
+	  
+	  $scope.deletePost = function(post){
+	    if (confirm('Are you sure?') == true) {
+	      $http.delete(config.url + 'posts/'+post.id  );
+	      var index = $scope.posts.indexOf(post);
+	      $scope.posts.splice(index,1);
+	    }
+	  };
+	  
+	  $scope.editPost = function(post, index){
+	    $http.get(config.url + 'posts/' + post.id + '.json')
+	      .success(function(data, status, headers, config) {
+	        // this callback will be called asynchronously
+	        // when the response is available
+	        $scope.post = data;
+	        $scope.formState = "Update";
+	      })
+	      .error(function(data, status, headers, config) {
+	        // called asynchronously if an error occurs
+	        // or server returns response with status
+	        // code outside of the <200, 400) range
+	      });
+	  };
+	};
+});
